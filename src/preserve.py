557e00bfda0329adf31fb81ab8b15c67df3b4a1f
pepechu <qingyuy@andrew.cmu.edu>
2018-05-04 18:48:49 -0400

    removed unnecessary packages

--- src/preserve.py
+++ src/preserve.py
@@ -1,8 +1,6 @@
 # preserve color
 import numpy as np
 import cv2
-from PIL import Image
-from skimage import color
 
 # given 2 input uint8 images (content and gallery) of same size
 # returns the gallery image by preserving the color from content
@@ -11,5 +9,5 @@ def preserve_lab(content, gallery):
 	content_lab = cv2.cvtColor(content, cv2.COLOR_BGR2LAB)
 	gallery_lab = cv2.cvtColor(gallery, cv2.COLOR_BGR2LAB)
 	convert = content_lab
-	convert[..., 0] = gallery_lab[..., 0]
+	convert[..., 0] = gallery_lab[..., 0]	
 	return np.array(cv2.cvtColor(convert, cv2.COLOR_LAB2BGR), 'uint8');
