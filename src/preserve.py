dd06f174600603e0d61607f53aff686a900f3ca1
pepechu <qingyuy@andrew.cmu.edu>
2018-05-04 18:44:56 -0400

    change package for more steady result

--- src/preserve.py
+++ src/preserve.py
@@ -1,5 +1,6 @@
 # preserve color
 import numpy as np
+import cv2
 from PIL import Image
 from skimage import color
 
@@ -7,9 +8,8 @@ from skimage import color
 # returns the gallery image by preserving the color from content
 # by keeping luminosity of LAB color space of gallery and original color
 def preserve_lab(content, gallery):
-	content_lab = color.rgb2lab(content)
-	gallery_lab = color.rgb2lab(gallery)
+	content_lab = cv2.cvtColor(content, cv2.COLOR_BGR2LAB)
+	gallery_lab = cv2.cvtColor(gallery, cv2.COLOR_BGR2LAB)
 	convert = content_lab
 	convert[..., 0] = gallery_lab[..., 0]
-	return np.array(color.lab2rgb(convert)*256, 'uint8');
-
+	return np.array(cv2.cvtColor(convert, cv2.COLOR_LAB2BGR), 'uint8');
